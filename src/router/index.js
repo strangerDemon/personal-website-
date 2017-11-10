90e9139cef567ba86bd7b0534b7e3e83cee6b23e
strangerDemon <735359571@qq.com>
2017-11-10 17:03:44 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,11 @@ import Vue from "vue";
 import Router from "vue-router";
 import Index from "@/views/Index";
 import particles from "@/components/particles/particles";
-import balloon from "@/components/balloon/balloon";
-import firefly from "@/components/fireFly/firefly";
-import heartLoading from "@/components/heartLoading/loading";
+import balloon from "@/components/c3Animation/balloon/balloon";
+import firefly from "@/components/c3Animation/fireFly/firefly";
+import heartLoading from "@/components/c3Animation/heartLoading/loading";
+
+import demo1 from "@/components/h5Canvas/demo1/index"
 Vue.use(Router);
 
 export default new Router({
@@ -36,6 +38,11 @@ export default new Router({
       path: "/heartLoading",
       name: "heartLoading",
       component: heartLoading
+    },
+    {
+      path:"/demo1",
+      name:"demo1",
+      component:demo1
     }
   ]
 });
